eb6b282d516e34585e8d84c4d6f78781e4591315
Sergey <smirsand@mail.ru>
2024-01-12 17:16:13 +0300

    add models.py product

--- product/models.py
+++ product/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
+from links.models import Factory
+
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
     """
     Модель продукта.
     """
-
     product_name = models.CharField(max_length=150, verbose_name='название')
     model = models.CharField(max_length=150, verbose_name='модель')
     release_date = models.DateTimeField(verbose_name='время выхода на рынок')
+    manufacturer = models.ForeignKey(Factory, on_delete=models.CASCADE, verbose_name='производитель')
 
     def __str__(self):
-        return self.product_name, self.model
+        return f"{self.product_name} {self.model}"
 
     class Meta:
         verbose_name = 'продукт'
