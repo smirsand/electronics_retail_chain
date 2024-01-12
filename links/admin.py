50bb06cdac52b5c78e4c2acfff176cf71673c12c
Sergey <smirsand@mail.ru>
2024-01-12 17:16:54 +0300

    add admin.py links

--- links/admin.py
+++ links/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from links.models import Factory, Retail, IE
+
+
+@admin.register(Factory)
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'street', 'house_number',)
+    list_filter = ('country',)
+    # readonly_fields = ('level',)
+
+
+@admin.register(Retail)
+class RetailAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'street', 'house_number',)
+    list_filter = ('country',)
+    # readonly_fields = ('level',)
+
+
+@admin.register(IE)
+class IEAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'street', 'house_number',)
+    list_filter = ('country',)
+    # readonly_fields = ('level',)
