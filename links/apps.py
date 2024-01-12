a080ba6e9097221a01327a12ac8b6aae2150d95f
Sergey <smirsand@mail.ru>
2024-01-12 12:26:18 +0300

    add files app links

--- links/apps.py
+++ links/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FactoryConfig(AppConfig):
+class LinksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'factory'
+    name = 'links'
