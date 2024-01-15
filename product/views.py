d0d3a71022d96ade0308ea4c1d4069681d451a57
Sergey <smirsand@mail.ru>
2024-01-15 17:28:48 +0300

    add code views.py product

--- product/views.py
+++ product/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from product.models import Product
+
+
+class ProductListView(ListView):
+    """Контроллер списка продуктов/товаров."""
+
+    model = Product
+    template_name = 'product/home.html'
+    extra_context = {'title': 'Товары'}
