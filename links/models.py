710409e469bdaa7e038b61a6730ff4457ddb9fd2
Sergey <smirsand@mail.ru>
2024-01-15 17:21:29 +0300

    add code models.py links

--- links/models.py
+++ links/models.py
@@ -5,57 +5,29 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Link(models.Model):
     """
-    Родительская модель звена сети.
+    Модель звена сети.
     """
+
+    FACTORY = 'завод'
+    IE = 'индивидуальный предприниматель'
+    RETAIL = 'розничная сеть'
+
+    STATUS_CHOICES = [
+        (FACTORY, "завод"),
+        (IE, "индивидуальный предприниматель"),
+        (RETAIL, "розничная сеть"),
+    ]
+    link = models.CharField(max_length=50, choices=STATUS_CHOICES, verbose_name='звено сети')
     email = models.EmailField(unique=True, verbose_name='почта')
-    name = models.CharField(max_length=150, verbose_name='наименование')
+    name = models.CharField(max_length=150, verbose_name='название организации')
     country = models.CharField(max_length=150, verbose_name='страна')
     city = models.CharField(max_length=150, verbose_name='город')
     street = models.CharField(max_length=150, verbose_name='улица')
     house_number = models.IntegerField(verbose_name='номер дома')
 
     def __str__(self):
-        return f'{self.email}, {self.name}'
+        return f'{self.link}, {self.email}, {self.name}'
 
     class Meta:
         verbose_name = 'звено'
         verbose_name_plural = 'звено'
-
-
-class Factory(Link):
-    """
-    Модель завода.
-    """
-
-    def __str__(self):
-        return f'{self.email}, {self.name}'
-
-    class Meta:
-        verbose_name = 'завод'
-        verbose_name_plural = 'заводы'
-
-
-class IE(Link):
-    """
-    Модель индивидуального предпринимателя.
-    """
-
-    def __str__(self):
-        return f'{self.email}, {self.name}'
-
-    class Meta:
-        verbose_name = 'индивидуальный предприниматель'
-        verbose_name_plural = 'индивидуальные предприниматели'
-
-
-class Retail(Link):
-    """
-    Модель розничной сети.
-    """
-
-    def __str__(self):
-        return f'{self.email}, {self.name}'
-
-    class Meta:
-        verbose_name = 'розничная сеть'
-        verbose_name_plural = 'розничные сети'
