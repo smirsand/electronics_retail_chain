3ee077204b603c8147f3fcc52905556b87051b40
Sergey <smirsand@mail.ru>
2024-01-16 17:12:35 +0300

    add code models.py purchase

--- purchase/models.py
+++ purchase/models.py
@@ -8,17 +8,19 @@ class Purchase(models.Model):
     """
     Модель закупки товара.
     """
-    product_name = models.ForeignKey(Product, related_name='purchases_with_name', on_delete=models.CASCADE, verbose_name='наименование товара')
-    product_cost = models.ForeignKey(Product, related_name='purchases_with_cost', on_delete=models.CASCADE, verbose_name='стоимость товара')
-    quantity = models.IntegerField(verbose_name='количество товара')
-    supplier = models.ForeignKey(Link, related_name='purchases_as_supplier', on_delete=models.CASCADE, verbose_name='поставщик товара')
-    buyer = models.ForeignKey(Link, related_name='purchases_as_buyer', on_delete=models.CASCADE, verbose_name='покупатель товара')
-    data = models.DateTimeField(verbose_name='дата создания')
 
+    product_name = models.ForeignKey(Product, related_name='purchases_with_name', on_delete=models.CASCADE,
+                                     verbose_name='наименование товара')
+    quantity = models.IntegerField(verbose_name='количество товара')
+    supplier = models.ForeignKey(Link, related_name='purchases_as_supplier', on_delete=models.CASCADE,
+                                 verbose_name='поставщик товара')
+    buyer = models.ForeignKey(Link, related_name='purchases_as_buyer', on_delete=models.CASCADE,
+                              verbose_name='покупатель товара')
+    data = models.DateTimeField(verbose_name='дата создания')
     hierarchy = models.IntegerField(default=0, verbose_name='иерархия')
 
     def __str__(self):
-        return f"{self.product_name} {self.product_cost}"
+        return f"{self.product_name}"
 
     class Meta:
         verbose_name = 'закупка'
