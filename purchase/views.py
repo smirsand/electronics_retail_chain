aadf3d7df11ee0616afa003974ee7b4189469c8c
Sergey <smirsand@mail.ru>
2024-01-16 17:11:56 +0300

    add code views.py purchase

--- purchase/views.py
+++ purchase/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from purchase.models import Purchase
+from purchase.serializers import PurchaseSerializer
+
+
+class PurchaseCreateAPIView(generics.CreateAPIView):
+    """
+    Контроллер закупки продуктов/товаров.
+    """
+    model = Purchase
+    serializer_class = PurchaseSerializer
+
+    def perform_create(self, serializer):  # Увеличение иерархии +1. Иерархия максимум увеличивается до 3.
+        obj = serializer.save()
+        hierarchy = self.request.data.get('hierarchy')
+        if obj.hierarchy <= 2:
+            obj.hierarchy = hierarchy + 1
+        else:
+            obj.hierarchy = 3
+
+        obj.save()
+
+
+class PurchaseListAPIView(generics.ListAPIView):
+    """
+    Контроллер списка закупок продуктов/товаров.
+    """
+    serializer_class = PurchaseSerializer
+    queryset = Purchase.objects.all()
