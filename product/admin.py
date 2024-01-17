380887e096961627a85adb8a24d05a534fea271f
Sergey <smirsand@mail.ru>
2024-01-17 10:40:38 +0300

    add code admin.py product

--- product/admin.py
+++ product/admin.py
@@ -5,6 +5,6 @@ from product.models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'model', 'price', 'release_date', 'manufacturer',)
+    list_display = ('product_name', 'model', 'quantity', 'price', 'supplier', 'release_date',)
     list_filter = ('product_name', 'release_date',)
     readonly_fields = ('hierarchy',)
