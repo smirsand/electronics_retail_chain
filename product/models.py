f01baf59b260dfe30098af2b92dd5be3ed937876
Sergey <smirsand@mail.ru>
2024-01-17 13:51:37 +0300

    add code models.py product

--- product/models.py
+++ product/models.py
@@ -10,19 +10,21 @@ class Product(models.Model):
     Модель продукта.
     """
 
-    product_name = models.CharField(max_length=150, verbose_name='название')
+    name = models.CharField(max_length=150, verbose_name='название')
     model = models.CharField(max_length=150, verbose_name='модель')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='цена')
-    quantity = models.IntegerField(verbose_name='количество товара')
+    quantity = models.IntegerField(verbose_name='количество')
     release_date = models.DateTimeField(verbose_name='время выхода на рынок')
     supplier = models.ForeignKey(Link, limit_choices_to={'link': Link.FACTORY},
-                                     related_name='manufacturer_products', on_delete=models.CASCADE,
-                                     verbose_name='поставщик')
+                                 related_name='manufacturer_products', on_delete=models.CASCADE,
+                                 verbose_name='поставщик')
+    owner = models.ForeignKey(Link, limit_choices_to={'link': Link.FACTORY}, on_delete=models.CASCADE,
+                              verbose_name='владелец')
 
     hierarchy = models.IntegerField(default=0, verbose_name='иерархия')
 
     def __str__(self):
-        return f"{self.product_name} {self.model}"
+        return f"{self.name} {self.model}"
 
     class Meta:
         verbose_name = 'продукт'
