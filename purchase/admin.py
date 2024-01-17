0d1a2a2353203af0f7ddbb4dbba9695b0bc115a5
Sergey <smirsand@mail.ru>
2024-01-17 13:52:29 +0300

    add code admin.py purchase

--- purchase/admin.py
+++ purchase/admin.py
@@ -5,6 +5,5 @@ from purchase.models import Purchase
 
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'quantity', 'product_owner', 'buyer', 'data', 'hierarchy')
+    list_display = ('product_name', 'quantity', 'supplier', 'buyer', 'data')
     list_filter = ('product_name',)
-    readonly_fields = ('hierarchy',)
