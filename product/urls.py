90c56fa89a37dd3b124a2a9f3fb7c45768a8bd13
Sergey <smirsand@mail.ru>
2024-01-17 10:40:54 +0300

    add code urls.py product

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from product.apps import ProductConfig
-from product.views import ProductListView
+from product.views import ProductViewSet
 
 app_name = ProductConfig.name
 
+router = DefaultRouter()
+router.register(r'product', ProductViewSet, basename='product')
+
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
-]
+
+              ] + router.urls
