fcb2524f17b6c373f230f94b5e3f2d85687f2cfb
Sergey <smirsand@mail.ru>
2024-01-19 13:50:26 +0300

    add code models.py debt

--- debt/models.py
+++ debt/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from links.models import Link
+from product.models import Product
 
 
 class Debt(models.Model):
@@ -8,12 +9,13 @@ class Debt(models.Model):
     Модель задолженности.
     """
     duty = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='долг')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
     borrower = models.ForeignKey(Link, on_delete=models.CASCADE, related_name='borrowed_debt_set',
                                  verbose_name='заемщик')
     debtor = models.ForeignKey(Link, on_delete=models.CASCADE, related_name='owed_debt_set', verbose_name='должник')
 
     def __str__(self):
-        return f"{self.duty}"
+        return f"Долг {self.debtor} перед {self.borrower} - {self.duty}"
 
     class Meta:
         verbose_name = 'долг'
