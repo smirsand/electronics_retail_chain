b88a417a00fba516049487976c40e3b21a3bc48f
Sergey <smirsand@mail.ru>
2024-01-19 19:25:55 +0300

    add code urls.py users

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
-from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from users.apps import UsersConfig
-from users.views import RegisterView, ProfileView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('profile/', ProfileView.as_view(), name='profile'),
-
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
