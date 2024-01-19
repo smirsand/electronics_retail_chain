13279d421c7b1cff51e96744c9bad7565fb2620e
Sergey <smirsand@mail.ru>
2024-01-19 14:13:27 +0300

    add code urls.py debt

--- debt/urls.py
+++ debt/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from debt.apps import DebtConfig
-from debt.views import DebtListAPIView
+from debt.views import DebtListAPIView, DebtRetrieveAPIView, DebtDestroyAPIView
 
 app_name = DebtConfig.name
 
 urlpatterns = [
-    path('debts/list/', DebtListAPIView.as_view(), name='debt_list'),
+    path('debts/list/', DebtListAPIView.as_view(), name='debt-list'),
+    path('debts/<int:pk>/', DebtRetrieveAPIView.as_view(), name='debt-retrieve'),
+    path('debts/delete/<int:pk>/', DebtDestroyAPIView.as_view(), name='debt-destroy'),
+
 ]
