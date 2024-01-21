c5000fd10a3e11c4cbf39f179d7b117f9c439501
Sergey <smirsand@mail.ru>
2024-01-21 21:57:51 +0300

    add code views.py purchase

--- purchase/views.py
+++ purchase/views.py
@@ -1,7 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
 from debt.models import Debt
 from debt.permissions import IsNotSuperuser
