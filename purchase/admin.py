857ac743792de224241676c8297a475cc4280728
Sergey <smirsand@mail.ru>
2024-01-21 10:23:05 +0300

    add code admin.py purchase

--- purchase/admin.py
+++ purchase/admin.py
@@ -5,5 +5,5 @@ from purchase.models import Purchase
 
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'quantity', 'supplier', 'buyer', 'data')
+    list_display = ('product_name', 'quantity', 'supplier', 'buyer', 'data', 'owner',)
     list_filter = ('product_name',)
