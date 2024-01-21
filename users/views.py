30bd1a2b936bc3fb3ec7482cabf860b264d67fd1
Sergey <smirsand@mail.ru>
2024-01-21 10:24:15 +0300

    add code views.py users

--- users/views.py
+++ users/views.py
@@ -1,33 +1,17 @@
-from django.contrib.auth.views import LoginView as BaseLoginView
-from django.contrib.auth.views import LoginView as BaseLogoutView
-from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from users.forms import UserRegisterForm, UserProfileForm
 from users.models import User
+from users.serializers import UserSerializer, MyTokenObtainPairSerializer
 
 
-class LoginView(BaseLoginView):
-    template_name = 'users/login.html'
-    success_url = reverse_lazy("users:login")
+class UserCreateAPIView(generics.CreateAPIView):
+    """
+    Контроллер создания пользователя.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
-class LogoutView(BaseLogoutView):
-    template_name = 'users/login.html'
-    success_url = reverse_lazy("product:home")
-
-
-class RegisterView(CreateView):
-    model = User
-    form_class = UserRegisterForm
-    template_name = 'users/register.html'
-    success_url = reverse_lazy('users:login')
-
-
-class ProfileView(UpdateView):
-    model = User
-    form_class = UserProfileForm
-    success_url = reverse_lazy('users:profile')
-
-    def get_object(self, queryset=None):  # Для редактирования текущего пользователя.
-        return self.request.user
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
