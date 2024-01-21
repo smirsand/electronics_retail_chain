52c510eefff340ac6e0cedbdda9d2d3df3ad22b9
Sergey <smirsand@mail.ru>
2024-01-21 21:32:07 +0300

    add code views.py product

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from product.models import Product
+from product.paginators import ProductPagination
 from product.serializers import ProductSerializer
 
 
@@ -9,3 +10,4 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     filter_backends = [DjangoFilterBackend,]
     filterset_fields = ('name', 'quantity', 'city', 'country',)  # Набор полей для фильтрации
+    pagination_class = ProductPagination
